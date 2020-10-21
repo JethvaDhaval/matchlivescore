f13f9261e77b3de58f361b02f26da1e52eba1ffa
JethvaDhaval <dcjethva2525@gmail.com>
2020-10-21 16:22:18 +0530

    Updates

---
+++ precache-manifest.0f8cd77975aa90fcccdbd83eebbe7905.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f757781501837a69493ccd4261a6f7f4",
+    "url": "/matchlivescore/index.html"
+  },
+  {
+    "revision": "6de4ece3a759e5028f7e",
+    "url": "/matchlivescore/static/css/main.3e98d283.chunk.css"
+  },
+  {
+    "revision": "ee97414b72829f58984f",
+    "url": "/matchlivescore/static/js/2.bb674a76.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/matchlivescore/static/js/2.bb674a76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6de4ece3a759e5028f7e",
+    "url": "/matchlivescore/static/js/main.d1a5742e.chunk.js"
+  },
+  {
+    "revision": "dc8424e85d1195d41b33",
+    "url": "/matchlivescore/static/js/runtime-main.82767a9c.js"
+  },
+  {
+    "revision": "f739b6cc80e851fa7cb8f4ddb7b120f6",
+    "url": "/matchlivescore/static/media/vs.f739b6cc.jpg"
+  }
+]);
